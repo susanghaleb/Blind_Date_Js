1bd5f6759aaa065e16b85804dc2b402a1347eb31
Susanne Ghaleb <susanne@kelmans-MacBook-Pro.local>
2022-04-16 00:48:00 -0300

    Refactoring code separating and reordering functions

--- js/script.js
+++ js/script.js
@@ -1,103 +1,125 @@
-var users = [
-    {
-        name: 'Lucy',
-        gender: 'F',
-        hobby: 'pets',
-        avatar: 'avatar1.png'        
-    },
-    {
-        name: 'Betty',
-        gender: 'F',
-        hobby: 'pets',
-        avatar: 'avatar2.png'
-    },
-    {
-        name: 'Ronald',
-        gender: 'M',
-        hobby: 'music',
-        avatar: 'avatar3.png'
-    },
-    {
-        name: 'Christopher',
-        gender: 'M',
-        hobby: 'sports',
-        avatar: 'avatar4.png'
-    },
-    {
-        name: 'Ximena',
-        gender: 'F',
-        hobby: 'reading',
-        avatar: 'avatar5.png'
-    },
-    {
-        name: 'Paul',
-        gender: 'M',
-        hobby: 'shopping',
-        avatar: 'avatar6.png'
-    },
-    {
-        name: 'Charlie',
-        gender: 'M',
-        hobby: 'pets',
-        avatar: 'avatar7.png'
-    },
-];
+(() => {
+    'use strict';
+    var users = [
+        {
+            name: 'Lucy',
+            gender: 'F',
+            hobby: 'pets',
+            avatar: 'avatar1.png'
+        },
+        {
+            name: 'Betty',
+            gender: 'F',
+            hobby: 'pets',
+            avatar: 'avatar2.png'
+        },
+        {
+            name: 'Ronald',
+            gender: 'M',
+            hobby: 'music',
+            avatar: 'avatar3.png'
+        },
+        {
+            name: 'Christopher',
+            gender: 'M',
+            hobby: 'sports',
+            avatar: 'avatar4.png'
+        },
+        {
+            name: 'Ximena',
+            gender: 'F',
+            hobby: 'reading',
+            avatar: 'avatar5.png'
+        },
+        {
+            name: 'Paul',
+            gender: 'M',
+            hobby: 'shopping',
+            avatar: 'avatar6.png'
+        },
+        {
+            name: 'Charlie',
+            gender: 'M',
+            hobby: 'pets',
+            avatar: 'avatar7.png'
+        },
+    ];
 
-window.addEventListener('load', function() {
-    
-    function search() {
-        
-        //get hobby
-        var hobbyField = document.getElementById('hobby');
-        var hobby = hobbyField.value;
-        
-        //get gender
+
+
+
+    function render(filterUsers) {
+        document.getElementById('results').innerHTML = filterUsers();
+
+    }
+
+    function load() {
+        console.log("load")
+
+        // Variables globales
+
         var genderField = document.getElementById('gender');
+        var hobbyField = document.getElementById('hobby');
+        var searchBtn = document.getElementById('searchBtn');
         var selected = genderField.selectedIndex;
         var gender = genderField.options[selected].value;
-        console.log(gender);
-        function handleChange(event){
-            console.log(event.target.value)
+        var hobby = hobbyField.value;
 
-
-        
+        var search = () => {
+            render(filterUsers)
         }
 
-        function renderResult(avatar, name, hobby){
-            return(
-                '<div class="person-row">\
-                                   <img src="images/' + avatar + '" />\
-                                   <div class="person-info">\
-                                   <div>' + name + '</div>\
-                                   <div>' + hobby + '</div></div>\
-                                    <button>Add as friend</button></div>'
+        // Listener
+        searchBtn.addEventListener('click', search);
+        genderField.addEventListener('change', handleChangeGender)
 
-            )
-        }
-        function filterUsers(){
+
+        // Métodos
+
+        function filterUsers() {
             var resultsHTML = '';
             var numUsers = users.length;
-            
-            for(var i = 0; i < numUsers; i++) {
-                
-                if (gender == 'A' || gender == users[i].gender) {             
-                    if (hobby == '' || hobby == users[i].hobby) {
-                        resultsHTML += renderResult(users[i].avatar, users[i].name, users[i].hobby)
+            for (let index = 0; index < numUsers; index++) {
+                const element = users[index];
+                if (gender == 'A' || gender == element.gender) {
+                    if (hobby == '' || hobby == element.hobby) {
+                        resultsHTML += renderItem(element.avatar, element.name, element.hobby)
                     }
                 }
+
             }
-        return resultsHTML;    
-            
+
+            return resultsHTML
         }
-        genderField.addEventListener('change',handleChange)
-       
-     
-        
-        results.innerHTML = filterUsers();  
-    };
-    
-    var results = document.getElementById('results');
-    var searchBtn = document.getElementById('searchBtn');
-    searchBtn.addEventListener('click', search);
-    search();
-});
+
+        function renderItem(avatar, name, hobby) {
+            return (
+                `
+                <div class="person-row">
+                    <img src="images/${avatar}" />
+                    <div class="person-info">
+                        <p>${name}</p>
+                        <p>${hobby}</p>
+                    </div>
+                    <button>Add as friend</button></div>
+                </div>
+                `
+            )
+        }
+
+        function handleChangeGender(event) {
+
+            gender = event.target.value
+            render(filterUsers)
+        }
+
+        render(filterUsers)
+
+    }
+
+
+
+    window.addEventListener('load', load)
+
+
+})()
